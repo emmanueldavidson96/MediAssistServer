5dc5966a319534a421655a601ae8b4bf7bbd191b
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-16 02:35:23 +0100

    Latest

--- Index.js
+++ Index.js
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use("/", async (req, res) => {
-    res.send("Live")
+    res.send("Live Api Deployments")
 })
+
 
 app.listen(PORT, async () => {
     console.log(`App running in port ${PORT}`)
