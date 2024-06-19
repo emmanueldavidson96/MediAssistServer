fa70e8b48a59f28873949ecfc67f675a61db95fb
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-19 09:27:30 +0100

    Medi Assist Server

--- Index.js
+++ Index.js
@@ -5,7 +5,6 @@ const {GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,} = require("@google/generative-ai");
 const fs = require("fs");
-const express = require("express");
 const cors = require("cors");
 
 const port = process.env.PORT || 5000
