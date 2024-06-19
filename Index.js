e6e485440d5d6d31789af2884ed14609483a92ac
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-19 09:14:31 +0100

    Medi Assist Server

--- Index.js
+++ Index.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const {GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,} = require("@google/generative-ai");
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 const app = express();
+app.use(express.json())
+app.use(cors());
+dotenv.config()
+
+const my_google_ai = new GoogleGenerativeAI("AIzaSyCszGsD5xZuPNNKD2wPovdvBnzQ1Ci6B3o");
 
 app.use("/", async (req, res) => {
     res.send("Live Api Deployments")
 })
 
+app.use("/mediassist", async (req,res) => {
+    const model = my_google_ai.getGenerativeModel({
+        model: "gemini-1.5-flash"
+    })
+    const chat = model.startChat({
+        history: req.body.history
+    })
+    const msg = req.body.message
+    const result = await chat.sendMessage(msg)
+    const response = result.response
+    const text = response.text()
+    res.send(text)
+})
 
-app.listen(PORT, async () => {
-    console.log(`App running on port ${PORT}`)
+app.listen(port, async () => {
+    console.log(`App running on port ${port}`)
 })
